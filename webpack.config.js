1e749cb7f9b20174724d6c1f706fcda02ffd8fdb
Shiva Naru <shiva.naru@blueshieldca.com>
2019-04-05 16:06:48 -0700

    web pack 4 initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+// webpack v4
+const path = require('path');
+// update from 23.12.2018
+const nodeExternals = require('webpack-node-externals');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = {
+  entry: { main: './src/app.js' },
+  output: {
+    path: path.resolve(__dirname, 'wwwroot'),
+    filename: 'main.js'
+  },
+  target: 'node', // update from 23.12.2018
+  externals: [nodeExternals()], // update from 23.12.2018
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract(
+          {
+            fallback: 'style-loader',
+            use: ['css-loader']
+          })
+      }
+    ]
+  }, 
+  plugins: [
+    new ExtractTextPlugin("styles.css"),
+  ]
+};
